82c0c9ab6ce73fbba682278a9f8c8207f4dcd228
lm902 <i@lm902.cn>
2019-12-06 11:39:04 -0800

    Support server without rewrite capability

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/views/Home'
 import About from './components/views/About'
 
 export default class App extends React.PureComponent {
   render () {
-    return <BrowserRouter>
+    return <HashRouter hashType='hashbang'>
       <Layout>
         <Switch>
           <Route exact path='/'>
@@ -17,6 +17,6 @@ export default class App extends React.PureComponent {
           </Route>
         </Switch>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   }
 }
