6dcb60ed1761d5bc9e82170aa97de04ab4484481
lm902 <i@lm902.cn>
2019-12-06 10:41:51 -0800

    Move ICP to settings file

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navigation from './partial/Navigation'
+import { icp } from '../appSettings.json'
 
 export default class Layout extends React.PureComponent {
   render () {
@@ -14,7 +15,7 @@ export default class Layout extends React.PureComponent {
       <footer className='container-fluid'>
         <img alt='TMDb logo' style={{width: 70}} src='https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg' />
         <p className='text-muted'>*This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
-        <p className='text-muted'>黔ICP备17008543号-1</p>
+        <p className='text-muted'>{icp}</p>
       </footer>
     </div>
   }
