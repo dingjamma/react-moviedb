b75e86e4435eb55d16c21a13ac7077bf57ad2532
lm902 <i@lm902.cn>
2019-12-06 10:38:20 -0800

    Move content

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,6 +10,11 @@ export default class Layout extends React.PureComponent {
       <main className='container-fluid' style={{paddingTop: 10}}>
         {this.props.children}
       </main>
+      <hr />
+      <footer className='container-fluid'>
+        <img alt='TMDb logo' style={{width: 70}} src='https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg' />
+        <p className='text-muted'>*This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+      </footer>
     </div>
   }
 }
--- src/components/views/About.tsx
+++ src/components/views/About.tsx
@@ -4,9 +4,7 @@ export default class About extends React.PureComponent {
   render () {
     return <>
       <p>This project is the final assignment for SSDP React.</p>
-      <p>It is a site that you can browse the movie library provided by:</p>
-      <img alt='TMDb logo' style={{width: 100}} src='https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg' />
-      <p className='text-muted'>*This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+      <p>It is a site that you can browse, search, and read more about movies in the movie library.</p>
     </>
   }
 }
