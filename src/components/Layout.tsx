a8c8dd21df21addc4d29ef4e9238ed3be43ea6b1
lm902 <i@lm902.cn>
2019-12-06 10:39:21 -0800

    Add Internet Content Provider license number

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,6 +14,7 @@ export default class Layout extends React.PureComponent {
       <footer className='container-fluid'>
         <img alt='TMDb logo' style={{width: 70}} src='https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg' />
         <p className='text-muted'>*This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+        <p className='text-muted'>黔ICP备17008543号-1</p>
       </footer>
     </div>
   }
