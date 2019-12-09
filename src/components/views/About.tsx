e3a4d7463ae968290515a8f65ee201670df6fb83
lm902 <i@lm902.cn>
2019-12-09 11:37:09 -0800

    Add GitHub link

--- src/components/views/About.tsx
+++ src/components/views/About.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { repo } from '../../appSettings.json'
 
 export default class About extends React.PureComponent {
   render () {
     return <>
       <p>This project is the final assignment for SSDP React.</p>
       <p>It is a site that you can browse, search, and read more about movies in the movie library.</p>
+      <Button variant='info' href={repo}>Browse source</Button>
     </>
   }
 }
