60c70a791c614305c49c4d4d099807221a438555
lm902 <i@lm902.cn>
2019-12-09 11:47:37 -0800

    Add tagline and bug fixes

--- src/components/views/Detail.tsx
+++ src/components/views/Detail.tsx
@@ -33,7 +33,8 @@ export default class Detail extends React.Component {
         {movie.homepage && <Button variant='success' className='ml-1' href={movie.homepage}>Homepage</Button>}
         <FavoriteButton id={movie.id} />
         <h1>{movie.title}</h1>
-        {movie.original_title !== movie.title && <h2>movie.original_title</h2>}
+        {movie.original_title !== movie.title && <h2>{movie.original_title}</h2>}
+        {movie.tagline && <h4>{movie.tagline}</h4>}
         <Row>
           {
             movie.poster_path &&
