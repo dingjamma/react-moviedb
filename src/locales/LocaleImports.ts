96e9c8949ebe546f6dbd532d811227f7aa213535
lm902 <i@lm902.cn>
2019-12-31 23:20:03 -0500

    Add support for zh_TW

--- src/locales/LocaleImports.ts
+++ src/locales/LocaleImports.ts
@@ -1,5 +1,6 @@
 import en_US from './en_US'
 import zh_CN from './zh_CN'
+import zh_TW from './zh_TW'
 
 export interface LocaleDefinition {
   nav_home: string
@@ -50,7 +51,7 @@ export interface LocaleDefinition {
 }
 
 export const locales: { [index: string]: LocaleDefinition } = {
-  en_US, en: en_US, zh_CN, zh: zh_CN
+  en_US, en: en_US, zh_CN, zh: zh_CN, zh_TW, zh_HK: zh_TW
 }
 
 export class LocaleImports {
